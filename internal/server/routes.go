4de2c927203b42e8599ad889600943d3c63fbe7c
Dubjay18 <jejeniyi7@gmail.com>
2024-12-08 21:18:09 +0100

    fix

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -34,7 +34,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp := make(map[string]string)
-	resp["message"] = "Hello Wkllorld"
+	resp["message"] = "Hello World"
 
 	c.JSON(http.StatusOK, resp)
 }
